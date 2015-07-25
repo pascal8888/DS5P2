8a582254003df8553d2240c8149fdef557e0ecfe
James Schacht <pascal888@yahoo.com>
2015-07-24 22:15:38 -0400

    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- scrapples.R
+++ scrapples.R
@@ -9,3 +9,18 @@ human.impact.plot <-
     xlab("Weather Event")
 tmp <- qplot(df_people_fatalities$FATALITIES[1:10],df_people_fatalities$EVTYPE[1:10],xlab="Number Fatalities",ylab="Event Type",main="Top 10 Fatal Weather Events 1996/01/01 - 2011/11/30",color="red", size=10)
 tmp + theme(legend.position="none")
+find_mplyr <- function(x) {
+    if (x == "K") {
+        mplyr <- 1000
+    } else if (x == "M") {
+        mplyr <- 1000000
+    } else if (x == "B") {
+        mplyr <- 1000000000
+    } else if (x == "0") {
+        mplyr <- 1
+    } else {
+        mplyr <- NA
+    }
+    return <- mplyr
+}
+df_property <- mutate(df_property,df_property$PROPDMG * find_mplyr(df_property$PROPDMGEXP))
